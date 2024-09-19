055e4ab3c424c34b9fdc2f463104f43ac6eed1ed
katushenca <shutenko.katya@bk.ru>
2024-09-20 00:59:08 +0500

    adding gui elements
    adding:
    - background
    - dice table
    - dice button
    
    fixing position and sizes of all exsiting elements
    
    Window size: 1280 x 720

---
+++ image_settings.py
@@ -0,0 +1,25 @@
+WIDTH = 1280
+HEIGHT = 720
+
+images_data = {
+    "background": {
+        "width": WIDTH,
+        "height": HEIGHT,
+        "pos": (0, 0)
+    },
+    "field": {
+        "width": HEIGHT,
+        "height": HEIGHT,
+        "pos": (100, 0)
+    },
+    "dice_table": {
+        "width": 328,
+        "height": 297,
+        "pos": (WIDTH-400, HEIGHT // 3)
+    },
+    "dice_button": {
+        "width": 400,
+        "height": 200,
+        "pos": (WIDTH - 440, HEIGHT - 200)
+    },
+}
--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 import pygame
-import Screen
+import image_settings
 
 pygame.init()
-background = pygame.image.load("Assets/field.jpg")
 
-screen = Screen.Screen()
-while True:
-    screen.screen.blit(background, (0, 0))
+# window size settings
+screen_width = image_settings.WIDTH
+screen_height = image_settings.HEIGHT
+screen = pygame.display.set_mode((screen_width, screen_height))
 
+# game elements initialization
+bg_image = pygame.image.load("Assets/background.jpg")
+img_data = image_settings.images_data
+background = pygame.transform.scale(bg_image,
+                                    (img_data["background"]["width"],
+                                     img_data["background"]["height"]))
+
+dice_table_image = pygame.image.load("Assets/dice_table.png")
+dice_table = pygame.transform.scale(dice_table_image,
+                                    (img_data["dice_table"]["width"],
+                                     img_data["dice_table"]["height"]))
+
+field_image = pygame.image.load("Assets/field.png")
+field = pygame.transform.scale(field_image,
+                               (img_data["field"]["width"],
+                                img_data["field"]["height"]))
+
+dice_button_image = pygame.image.load("Assets/dice_button.png")
+dice_button = pygame.transform.scale(dice_button_image,
+                                     (img_data["dice_button"]["width"],
+                                      img_data["dice_button"]["height"]))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+
+    # adding game elements to screen
+    screen.blit(background, img_data["background"]["pos"])
+    screen.blit(field, img_data["field"]["pos"])
+    screen.blit(dice_table, img_data["dice_table"]["pos"])
+    screen.blit(dice_button, img_data["dice_button"]["pos"])
+
+    pygame.display.flip()
+
+pygame.quit()
